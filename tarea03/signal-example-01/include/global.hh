cd2f05f09a4e34fc00679ae6634ffe1a191bd2a7
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2021-10-21 17:58:35 -0300

    Add files via upload

---
+++ tarea03/signal-example-01/include/global.hh
@@ -0,0 +1,17 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+#include <random>
+#include <iostream>    // for cout
+#include <fstream>
+#include <queue>       // for queue
+#include <vector>      // for vector
+#include <array>
+
+#include <future>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+
+#include <csignal>
+
+#endif
